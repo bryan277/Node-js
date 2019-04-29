c8b2ef456f1dd4d08c79f7ffa21c5e76722fc336
hfvos90 <hfvos90@gmail.com>
2019-04-29 13:12:28 -0700

    #

--- greet/index.js
+++ greet/index.js
@@ -1,7 +1,11 @@
 var english = require('./english');
 var espanol = require('./espanol');
+var german = require('./german');
+var chinese = require('./chinese');
 
 module.exports = {
     english: english,
-    espanol: espanol
+    espanol: espanol,
+    german: german,
+    chinese: chinese
 };
--- require.js
+++ require.js
@@ -2,6 +2,9 @@ var greet = require('./greet');
 
 greet.english();
 greet.espanol();
-
+greet.german();
+greet.chinese();
 // Hello
 // Hola
+// Hallo
+// nihao
